0da28047b6fb00b15f71b007c844f36ae997e98f
Fullchee Zhang <fullchee@gmail.com>
2019-05-04 16:33:58 -0400

    Update row size for chariot

--- src/pieces/rook.js
+++ src/pieces/rook.js
@@ -1,14 +1,16 @@
 import Piece from './piece.js';
 
+const ROW_SIZE = 9;
+
 export default class Rook extends Piece {
   constructor(player){
     super(player, (player === 1 ? "https://upload.wikimedia.org/wikipedia/commons/2/2e/Xiangqi_rl1.svg" : "https://upload.wikimedia.org/wikipedia/commons/f/f7/Xiangqi_rd1.svg"));
   }
 
   isMovePossible(src, dest){
-    let mod = src % 8;
-    let diff = 8 - mod;
-    return (Math.abs(src - dest) % 8 === 0 || (dest >= (src - mod) && dest < (src + diff)));
+    let mod = src % ROW_SIZE;
+    let diff = ROW_SIZE - mod;
+    return (Math.abs(src - dest) % ROW_SIZE === 0 || (dest >= (src - mod) && dest < (src + diff)));
   }
 
   /**
@@ -27,9 +29,9 @@ export default class Rook extends Piece {
       pathStart = src;
       pathEnd = dest;
     }
-    if(Math.abs(src - dest) % 8 === 0){
-      incrementBy = 8;
-      pathStart += 8;
+    if(Math.abs(src - dest) % ROW_SIZE === 0){
+      incrementBy = ROW_SIZE;
+      pathStart += ROW_SIZE;
     }
     else{
       incrementBy = 1;
