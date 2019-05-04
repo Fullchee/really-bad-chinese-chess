546fb8d2c72fd923795b4438bfbfb7f5eab4aede
Fullchee Zhang <fullchee@gmail.com>
2019-05-04 16:31:25 -0400

    Add behaviour for pawn

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -11,20 +11,20 @@ export default class Pawn extends Piece {
   }
 
   isMovePossible(src, dest, isDestEnemyOccupied){
-
     if(this.player === 1){
-      if((dest === src - 8 && !isDestEnemyOccupied) || (dest === src - 16 && this.initialPositions[1].indexOf(src) !== -1)){
+      if(dest === src - 9){
         return true;
       }
-      else if(isDestEnemyOccupied && (dest === src - 9 || dest === src - 7)){
+      // allowed if not 
+      else if(src < 45 && ((dest === src - 1 && src % 9 !== 0) || (dest === src + 1 && src % 9 !== 8))){
         return true;
       }
     }
     else if(this.player === 2){
-      if((dest === src + 8 && !isDestEnemyOccupied) || (dest === src + 16 && this.initialPositions[2].indexOf(src) !== -1)){
+      if (dest === src + 9) {
         return true;
       }
-      else if(isDestEnemyOccupied && (dest === src + 9 || dest === src + 7)){
+      else if (src >= 45 && ((dest === src - 1 && src % 9 !== 0) || (dest === src + 1 && src % 9 !== 8))) {
         return true;
       }
     }
@@ -38,12 +38,7 @@ export default class Pawn extends Piece {
    * @return {[type]}      [description]
    */
   getSrcToDestPath(src, dest){
-    if(dest === src - 16){
-      return [src - 8];
-    }
-    else if(dest === src + 16){
-      return [src + 8];
-    }
+    // no jumping
     return [];
   }
 }
